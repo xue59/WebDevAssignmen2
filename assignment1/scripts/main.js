685c0e0f5766cb94aee835a112e5c814f139d34d
Zack Xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-02-20 17:41:08 -0500

    part2 date/time display done to all pages with dynamically element added

--- assignment1/scripts/main.js
+++ assignment1/scripts/main.js
@@ -15,10 +15,6 @@ function getCurrentTime(){
     let final_time = `${hrs}:${mins}:${secs} ${AMPM}`;
     return final_time;
 }
-function updateTime(){
-    const p_time = document.querySelector('#time-display');
-    p_time.textContent = getCurrentTime();
-}
 
 function getCurrentDate(){
     let date = new Date();
@@ -31,18 +27,35 @@ function getCurrentDate(){
 
     return final_date;
 }
-function updateDate(){
-    const p_time = document.querySelector('#time-display');
-    p_time.textContent = getCurrentDate();
-}
 
+function updateDateTime(date_or_time){
+    //console.log("in to date time")
+    const p_td_display = document.querySelector('#td_display');
+    if (p_td_display == undefined){
+        let p_td_display=document.createElement('p');
+        p_td_display.setAttribute('id','td_display')
+        if(date_or_time == 'date'){
+            p_td_display.textContent = getCurrentDate();
+        } else if (date_or_time == 'time'){
+            p_td_display.textContent = getCurrentTime();
+        }
+        timer_section.appendChild(p_td_display);
+    }else{
+        if(date_or_time == 'date'){
+            p_td_display.textContent = getCurrentDate();
+        } else if (date_or_time == 'time'){
+            p_td_display.textContent = getCurrentTime();
+        }
+    }
+}
 
 
 var glob_timer;
 const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
 const weekdayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const p_time = document.querySelector('#time-display');
 const radios=document.querySelectorAll('input[name="time-date-btn"]'); 
+const timer_section = document.querySelector('.date-time-section');
+
 for(let rad of radios){
     rad.addEventListener('change', displayTimeDate);
 }
@@ -52,12 +65,12 @@ function displayTimeDate(event){
     clearInterval(glob_timer);
     if (event != undefined){
         if (event.target.value === "time"){
-            updateTime();
-            glob_timer = setInterval(updateTime, 1000);  // display the current Time and update every 1s 
+            updateDateTime('time');
+            glob_timer = setInterval("updateDateTime('time')", 1000);  // display the current Time and update every 1s 
 
         } else if (event.target.value === "date"){
-            updateDate();
-            glob_timer = setInterval(updateDate, 1000); // display the current Date and update every 1s 
+            updateDateTime('date');
+            glob_timer = setInterval("updateDateTime('date')", 1000); // display the current Date and update every 1s 
         } else {
             p_time.textContent = "Select to display the date and time";
         }
